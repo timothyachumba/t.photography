927d71492d27cab0861305b09dcd448f7cc3f78e
florianpnn <florian.peninon@gmail.com>
2017-01-13 09:36:11 +0100

    pictures folder is scrapped and json build

--- pictures-automation.js
+++ pictures-automation.js
@@ -1,9 +1,24 @@
 var fs = require('fs');
+var _ = require('lodash');
+var jsonPath = './src/pictures.json';
+var picturesPath = './src/images/photos/';
+var jsonData = {};
+// This is the URL that will be used for the pictures to load
+var relativeUrl = './';
 
-fs.writeFile('./src/pictures.json', 'Hello world!', function(error) {
+var categories = fs.readdirSync(picturesPath);
+categories.forEach(function(category) {
+  jsonData[category] = fs
+    .readdirSync(picturesPath + category)
+    .map(function(picture) {
+      return relativeUrl + picture;
+    });
+});
+
+fs.writeFile(jsonPath, JSON.stringify(jsonData, null, 2), function(error) {
   if (error) {
     console.log(`write error: ${error}`);
   } else {
     console.log('write to pictures.json successful');
   }
-})
+});
